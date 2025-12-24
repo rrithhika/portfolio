71b9f47fdc27f35b9fffb2a0d9a696952a23552e
rrithhika <ritheekaebay@gmail.com>
2025-12-24 10:52:29 +0530

    mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="nav-brand"></div>
+      <div className="nav-brand">Ritheeka</div>
       <div className="nav-links">
         <a href="#home">Home</a>
         <a href="#about">About</a>
